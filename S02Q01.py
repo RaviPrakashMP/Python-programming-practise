9746f720cf4658db01404d89b9d04df2926dcccc
RaviPrakashMP <54932206+RaviPrakashMP@users.noreply.github.com>
2019-10-07 22:32:58 +0530

    Add files via upload

---
+++ S02Q01.py
@@ -0,0 +1,6 @@
+MilesDriven = float(input("Enter the number of miles driven"))
+GallonsofGasUsed = float(input("Enter the gallons of gas used"))
+
+MilesPerGallon = MilesDriven/GallonsofGasUsed
+
+print("The car's miles-per-gallon is",str(MilesPerGallon),"miles/gallon")
