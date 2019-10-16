3bab70ea574ea625a8e7ee19efc9ceeaa3e479f0
RaviPrakashMP <54932206+RaviPrakashMP@users.noreply.github.com>
2019-10-16 21:28:01 +0530

    Add files via upload

---
+++ S04Q02.py
@@ -0,0 +1,20 @@
+E = int(input("Enter the marks scored in English exam:"))
+S = int(input("Enter the marks scored in Science exam:"))
+M = int(input("Enter the marks scored in Mathematics exam:"))
+
+percentage = ((E+S+M)/270)*100
+
+def marks_scored(percentage):
+    if percentage > 90:
+        print("The student has scored First Class")
+
+    elif 75 < percentage < 90:
+        print("The student has scored second class")
+
+    elif 35 < percentage < 75:
+        print("The student scored average")
+
+    else:
+        print("The student is failed")
+    
+marks_scored(percentage)
