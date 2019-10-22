a3f495ca069a6dcc67fd087be9a1f4fa7d0786cc
RaviPrakashMP <54932206+RaviPrakashMP@users.noreply.github.com>
2019-10-22 11:18:26 +0530

    Update S05Q03.py

--- S05Q03.py
+++ S05Q03.py
@@ -1,7 +1,4 @@
-#Take a number as input from the user.
-#Print all the squares of numbers from 1 to any large number. 
-#The numbers printed should be less than 
-#The number given as input by the user
+
 n = int(input("Enter a number:"))
 
 for a in range(1,n):
